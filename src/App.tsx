7e7757f2ffe4bf5082db5167163c1b89b968d789
Georgi Mavridis <georgimavridis21@gmail.com>
2022-04-01 23:10:12 +0300

    add nextjs 12 and static generation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import GamesList from "./components/GamesList";
 import Header from "./components/Header";
 import games from "./games";
---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../index.css";
+import "../App.css";
+import Head from "next/head";
+import { AppProps } from "next/app";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React App</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
+}
